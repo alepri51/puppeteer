c11b407a819df4c57c8b02420412725fc3ba763c
alepri51 <39708925+alepri51@users.noreply.github.com>
2019-01-26 18:05:52 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+
+(async () => {
+    //долгота = lat
+    //широта = lоt
+    let [lat, lon] = [37.598431, 55.749977]; //building_id
+    let building_id = [lat, lon];
+
+    let { data: { response: { GeoObjectCollection: { featureMember: metro = [] }}}} = await axios.get(`https://geocode-maps.yandex.ru/1.x/?apikey=3eb0b0b4-b25d-42a9-92ad-9c04dbd2e172&geocode=${lat},${lon}&kind=metro&results=3&format=json`);
+
+    metro = metro.map(({ GeoObject: geo }) => {
+        let [lat, lon] = geo.Point.pos.split(/\s/);
+
+        return {
+            name: geo.name,
+            description: geo.description,
+            coordinates: [+lat, +lon]
+        }
+    });
+
+    //console.log(metro);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(`file:///${__dirname}/map.html`);
+    //await page.goto('https://example.com');
+    
+    //await afterUrlOpen(page);
+    //await page.pdf({path: 'hn.pdf', format: 'A4'});
+
+    let res = await page.evaluate(async (metro, building_id) => {
+        
+        let routes = await GetMetroRoutes({ from: building_id, metro, types: ['auto', 'pedestrian', 'masstransit'] });
+        
+        return routes;
+
+    }, metro, building_id);
+
+    await page.screenshot({path: 'example.png'});
+
+    //console.log(res);
+
+    await browser.close();
+})();
